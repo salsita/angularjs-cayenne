79c9cd7e2ba16f7b776eb2e2545b994218974e9e
Nikolay Khodov <nikolay@salsitasoft.com>
2013-02-08 15:44:28 +0400

    Deleted old files that have already combined into notification.js

--- app/js/notification/notification-ctrl.js
+++
@@ -1,7 +0,0 @@
-controllers.controller('notification', function ($scope, $rootScope) {
-
-    // Keep notification messages aligned with $rootScope
-    $rootScope.$watch('notifications', function(newValue) {
-        $scope.notifications = newValue;
-    }, true);
-});
